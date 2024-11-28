644c5bed45bf6a2c8db0b9265f3dc1c2b19e24d5
GeSi <sdropoulos@gmail.com>
2024-11-28 21:23:11 +0200

    Finished Classroom Controller and Service + added the injection

--- src/main/java/com/springboot_mySB/mySB_artifacrt/MyTests/ClassController.java
+++ src/main/java/com/springboot_mySB/mySB_artifacrt/MyTests/ClassController.java
@@ -1,5 +1,6 @@
 package com.springboot_mySB.mySB_artifacrt.MyTests;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,9 +8,10 @@ import java.util.List;
 
 @RestController
 public class ClassController {
-
+    @Autowired
+    private ClassService classService;
     @RequestMapping("/classroom")
-    public String getClassroom(){
-        return "Test";
+    public List<Classroom> getClassroom(){
+        return classService.getAllClassrooms();
     }
 }
--- src/main/java/com/springboot_mySB/mySB_artifacrt/MyTests/ClassService.java
+++ src/main/java/com/springboot_mySB/mySB_artifacrt/MyTests/ClassService.java
@@ -3,19 +3,25 @@ package com.springboot_mySB.mySB_artifacrt.MyTests;
 import com.springboot_mySB.mySB_artifacrt.Topic.Topics;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class ClassService {
 
-    List<Classroom> 
+    List<Classroom> classrooms = new ArrayList<Classroom>();
     Classroom mathClassroom = new Classroom(20, 15, true, "Introduction to mathematics");
     Classroom geographyClassroom = new Classroom(23, 25, false, "Introduction to geography");
     Classroom physicsClassroom = new Classroom(17, 44, false, "Introduction to physics");
     Classroom literatureClassroom = new Classroom(34, 5, true, "Introduction to literature");
 
     public List<Classroom> getAllClassrooms() {
-        return topics;
+
+        classrooms.add(mathClassroom);
+        classrooms.add(geographyClassroom);
+        classrooms.add(physicsClassroom);
+        classrooms.add(literatureClassroom);
+        return classrooms;
     }
 
 }
